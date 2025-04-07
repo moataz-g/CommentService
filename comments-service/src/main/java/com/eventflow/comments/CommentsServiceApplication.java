1723ae161e03a3738df8e13a3eada68c5de870e2
moataz gouiaa <100675571+moataz-g@users.noreply.github.com>
2025-04-07 10:40:15 +0100

    .

--- comments-service/src/main/java/com/eventflow/comments/CommentsServiceApplication.java
+++ comments-service/src/main/java/com/eventflow/comments/CommentsServiceApplication.java
@@ -2,6 +2,7 @@ package com.eventflow.comments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 public class CommentsServiceApplication {

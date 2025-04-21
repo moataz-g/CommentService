e2610989b70a635390b1181c8faa6977a83bbc0e
moataz gouiaa <100675571+moataz-g@users.noreply.github.com>
2025-04-21 11:13:21 +0100

    .

--- comments-service/src/main/java/com/eventflow/comments/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventflow.comments.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.eventflow.comments.model.Comment;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
